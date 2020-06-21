8fd89ce8e44c85ee177be84cad6936871ea8ab28
Charlie Martinez <charlie.mrtnz@gmail.com>
2020-06-20 23:45:14 -0700

    Adding homework files

--- Pybank/main.py
+++ Pybank/main.py
@@ -11,42 +11,61 @@ import csv
 
 pybankin_csv = os.path.join("Pybank","Resources","budget_data.csv")
 
-# Data list 
-Total_Months = []
-Profit_loss = 0
-
-
-
-# Open and loop through data
+#Open and loop through data
 with open(pybankin_csv, 'r') as pybudget:
 
-# Parse data
+    # Set variables 
+    Total_Months = []
+    Profitloss_Total = []
+    Profit_loss = []
+    Profitloss_Total = 0
+    average_change = 0
+    year_change = []
+    change = 0
+   
+  
+    #Parse data
     csvreader = csv.reader(pybudget, delimiter=',')
 
-#Read the header data
+    #Read the header data
     pybankin_header = next(csvreader)
     for data in csvreader:
 
-# Store Data for Financial Analysis Summary
+        # Store Data for Financial Analysis Summary
         Total_Months.append(data[0])
-        Profit_loss.append(int(data[1])
-        
-        #AverageChange 
+        Profit_loss.append(int(data[1]))
+        Profitloss_Total += int(data[1]) 
+         
+    # Average change   
+    for i in range(1,len(Profit_loss)):
+        change = (Profit_loss[i]) - (Profit_loss[i-1])
+        year_change.append(change)  
+        Total_Change = (sum(year_change))
+                     
 
-
-# Calculations for Financial Analysis
+# Calculations for Financial Analysis Print
 TotalMonths = len(Total_Months)
-Total_ProfitLoss = sum(Profit_loss)   
-#Average_change = sum(Averagechange)/len(Averagechange)
-#Greatest_Increase = max(Averagechange)
-#Greatest_Decrease = min(Averagechange)
-
+Average_Change = round(Total_Change/((len(Total_Months))-1),2)
+Greatest_Increase = max(year_change)
+Greatest_Inc_Index = year_change.index(Greatest_Increase)+1
+Greatest_Decrease = min(year_change)
+Greatest_Dec_Index = year_change.index(Greatest_Decrease )+1
 
 # Display Financial Analysis Summary
 print("Financial Analysis")
-print("-----------------------------")
-print("Total Months: " + str(TotalMonths)) 
-#print("Total: $" + str(Total_Profitloss))
-#print("Average Change:  $" + str(Average_change))
-#print("Greatest Increase:  $" + str(Greatest_Increase))
-#print("Greatest Decrease:  $" + str(Greatest_Decrease))
+print("--------------------------")
+print(f"Total Months: {str(TotalMonths)}")
+print(f"Total: ${str(Profitloss_Total)}")
+print(f"Average Change: ${str(Average_Change)}")
+print(f"Greatest Increase: {Total_Months[Greatest_Inc_Index]} (${str(Greatest_Increase)})")
+print(f"Greatest Increase: {Total_Months[Greatest_Dec_Index]} (${str(Greatest_Decrease )})")
+
+analysis_file = os.path.join ("Pybank","Analysis","FinancialAnalysis.txt")
+with open(analysis_file, 'w') as FinancialAnalysis:
+    FinancialAnalysis.write("Financial Analysis\n")
+    FinancialAnalysis.write("--------------------------\n")
+    FinancialAnalysis.write(f"Total Months: {str(TotalMonths)}\n")
+    FinancialAnalysis.write(f"Total: ${str(Profitloss_Total)}\n")
+    FinancialAnalysis.write(f"Average Change: ${str(Average_Change)}\n")
+    FinancialAnalysis.write(f"Greatest Increase: {Total_Months[Greatest_Inc_Index]} (${str(Greatest_Increase)})\n")
+    FinancialAnalysis.write(f"Greatest Increase: {Total_Months[Greatest_Dec_Index]} (${str(Greatest_Decrease )})\n")
--- Pypoll/main.py
+++ Pypoll/main.py
@@ -10,39 +10,65 @@
 #Winner: Khan
 #-------------------------
 
-
-
 import os
 import csv
+
 pypoll_csv = os.path.join("Pypoll","Resources","election_data.csv")
 
-# Open and read csv file
-with open(pypoll_csv,'r' ) as csvfile:
-    csvreader = csv.reader(csvfile, delimiter=',')
+# Set variables 
+Voteid = []
+Results = []
 
-#Read the first row of the header
-    csv_header = next(csvfile)
- 
-    # Set variables 
-    Votes = []
-    Vote_Total = 0
-    
-    
-    #Read the header data
+
+# Open and read csv file
+with open(pypoll_csv,'r' ) as polldata:
+
+    csvreader = csv.reader(polldata, delimiter=',')
     pypoll_header = next(csvreader)
+   
     for data in csvreader:
 
-        # Store Data for Financial Analysis Summary
-        Votes.append(int(data[0])
+        # Store Data 
+        Voteid.append(data[0])
+        Candidates = data[2]
+        
+        # Unique Candidates
+        if Candidates not in Results: 
+            Results[Candidates] = 1
+        else: 
+            Results[Candidates] += 1
 
-     
-#Calc
+   
+TotalVotes = len(Voteid)
+winner = max(Results, key=Results.get)
+#UniqueCandidates = list(set(CandidateList))
+#CandidateVotes = CandidateList.count(data[2])
 
-Vote_Total = len(Votes)
+# Display Election Summary
+print("Election Results")
+print("----------------------")
+print(f"Total Votes: {str(TotalVotes)}")
+print("----------------------")
+print(f'Khan: {Results["Khan"] / TotalVotes:.3%} ({Results["Khan"]})')
+print(f'Correy: {Results["Correy"] / TotalVotes:.3%} ({Results["Correy"]})')
+print(f'Li: {Results["Li"] / TotalVotes:.3%} ({Results["Li"]})')
+print(f'O'Tooley: {Results["O'Tooley"] / TotalVotes:.3%} ({Results["O'Tooley"]})')
+print("----------------------")
+print(f"Winner: {str(winner)}")
+print("----------------------")
 
 
-print(Vote_Total)
- 
-
-
-
+analysis_file = os.path.join ("Pypoll","Analysis","ElectionAnalysis.txt")
+with open(analysis_file, 'w') as ElectionAnalysis:
+    ElectionAnalysis.write("Election Result\n")
+    ElectionAnalysis.write("--------------------------\n")
+    ElectionAnalysis.write(f"Total Votes: {str(TotalVotes)}\n")
+    ElectionAnalysis.write(f'Khan: {Results["Khan"] / TotalVotes:.3%} ({Results["Khan"]})\n')
+    ElectionAnalysis.write(f'Correy: {Results["Correy"] / TotalVotes:.3%} ({Results["Correy"]})\n')
+    ElectionAnalysis.write(f'Li: {Results["Li"] / TotalVotes:.3%} ({Results["Li"]})\n')
+    ElectionAnalysis.write(f'O'Tooley: {Results["O'Tooley"] / TotalVotes:.3%} ({Results["O'Tooley"]})\n')
+    ElectionAnalysis.write("----------------\n")
+    ElectionAnalysis.write("--------------------------\n")
+    ElectionAnalysis.write(f"Winner: {str(winner)}\n")
+    ElectionAnalysis.write("--------------------------\n")
+   
